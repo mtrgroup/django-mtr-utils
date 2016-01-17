cb3d0e4f6de9d4d51c224569c1378d999780eb7b
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-01-17 15:40:51 +0200

    Added ViewsCountMixin

--- mtr/utils/models.py
+++ mtr/utils/models.py
@@ -228,3 +228,32 @@ class TreeParentMixin(
 
     def __str__(self):
         return self.name
+
+
+# TODO: propertly define dependencies
+
+try:
+    from django.contrib.postgres.fields import ArrayField
+
+    class ViewsCountMixin(models.Model):
+        views_count = models.BigIntegerField(
+            _('views'), default=0, db_index=True)
+        views_ips = ArrayField(
+            models.CharField(
+                _('ips'), max_length=45), null=True, db_index=True)
+
+        class Meta:
+            abstract = True
+
+        def process_view(self, ip):
+            ips = self.views_ips or []
+            if ip not in ips:
+                ips.append(ip)
+
+                # TODO: use F()
+                self.views_ips = ips
+                self.views_count += 1
+                self.save(update_fields=['views_ips', 'views_count'])
+
+except ImportError:
+    pass
