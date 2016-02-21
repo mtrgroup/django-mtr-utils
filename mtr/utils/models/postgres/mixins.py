f85c615766a82a307ae98a2f726e84bb823d9841
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-02-21 22:36:49 +0200

    Added ImagesMixin without sorl

--- mtr/utils/models/postgres/mixins.py
+++ mtr/utils/models/postgres/mixins.py
@@ -24,3 +24,37 @@ class ViewsCountMixin(models.Model):
             self.views_ips = ips
             self.views_count += 1
             self.save(update_fields=['views_ips', 'views_count'])
+
+
+class ImagesMixin(models.Model):
+    image_list = ArrayField(
+        models.ImageField(_('image')), blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.image_list = []
+        for image in self.images.all():
+            try:
+                self.image_list.append(str(image.file))
+            except ValueError:
+                pass
+
+        super().save(*args, **kwargs)
+
+    @property
+    def main_image(self):
+        return self.image_list[0] if self.image_list else None
+
+    # def preview(self):
+    #     try:
+    #         thumbnail = get_thumbnail(self.main_image, '50x50')
+
+    #         return '<img src="{}" width="{}" height="{}">'.format(
+    #             thumbnail.url, thumbnail.width, thumbnail.height)
+    #     except:
+    #         return None
+    # preview.allow_tags = True
+    # preview.short_description = _('Preview')
+    # preview.admin_order_field = 'image_list'
