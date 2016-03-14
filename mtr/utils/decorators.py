bb10cb9abe52f65a3d76453610c76193f5450da9
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-03-14 19:12:59 +0200

    Added redirect_to param to anonymous decorator

--- mtr/utils/decorators.py
+++ mtr/utils/decorators.py
@@ -4,15 +4,13 @@ from django.conf import settings
 from django.shortcuts import redirect
 
 
-def anonymous_required(f):
+def anonymous_required(
+        f, redirect_to=getattr(settings, 'DEFAULT_AUTH_URL', '/')):
     """Redirect authorized user to his cabinet"""
-
-    # TODO: func to check where user should be redirected
 
     @wraps(f)
     def inner(request, *args, **kwargs):
         if request.user.is_authenticated():
-            return redirect(request.GET.get(
-                'next', getattr(settings, 'DEFAULT_AUTH_URL', '/')))
+            return redirect(redirect_to)
         return f(request, *args, **kwargs)
     return inner
