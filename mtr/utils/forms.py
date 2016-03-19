48d6b30d660e2d207ded8f76887e41b599da8d9d
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-03-19 23:26:29 +0200

    Added form mixin for widgets

--- mtr/utils/forms.py
+++ mtr/utils/forms.py
@@ -1,3 +1,8 @@
+from django import forms
+
+from .widgets import SelectizeCategoryWidget
+
+
 class GlobalInitialFormMixin(object):
 
     """Class initial params for instance initial kwarg"""
@@ -8,3 +13,20 @@ class GlobalInitialFormMixin(object):
         if kwargs.get('initial', None):
             kwargs['initial'].update(self.INITIAL)
         super(GlobalInitialFormMixin, self).__init__(*args, **kwargs)
+
+
+class SelectizeCategoryFormMixin(object):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        params = {}
+
+        if self.instance is not None:
+            params['group_id'] = self.instance.group_id
+
+        self.fields['linked_to'] = forms.ModelChoiceField(
+            label=self.fields['linked_to'].label,
+            required=self.fields['linked_to'].required,
+            queryset=self.Meta.model.objects.all(),
+            widget=SelectizeCategoryWidget(self.Meta.model, params=params))
