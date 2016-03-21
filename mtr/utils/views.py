b2b48068ee52a0f4e3c384a1b5fe900adf590110
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-03-21 19:36:20 +0200

    Added view for ajax responses

--- mtr/utils/views.py
+++ mtr/utils/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.admin.views.decorators import staff_member_required
 
 from mtr.utils.helpers import models_list, model_app_name
@@ -22,3 +22,20 @@ def model_label(request, name, pk):
                 item, 'admin_label', getattr(item, '__str__', ''))()
 
     return HttpResponse(result)
+
+
+@staff_member_required
+def model_resource(request, name):
+    fmodel = None
+    for model in models_list():
+        if name == model_app_name(model):
+            fmodel = model
+            break
+
+    result = []
+    query = request.GET.get('query', '')
+
+    if fmodel and hasattr(fmodel, 'process_resource') and query:
+        result = list(fmodel.process_resource(query, request))
+
+    return JsonResponse({'items': result})
