2e2ef030c44a631182f7e25fee1f88418eeaf9f8
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-04-16 23:55:08 +0300

    Removed django 1.6 support in urls

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -1,17 +1,10 @@
-import django
-
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 
-if django.get_version() < '1.7':
-    admin.autodiscover()
-
 urlpatterns = i18n_patterns(
-    '',
-
     url(r'^admin/', include(admin.site.urls)),
 )
 
